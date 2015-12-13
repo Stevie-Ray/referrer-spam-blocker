d8001e651b21f4e78aa339a5d4e766eadb0c8e50
Stevie-Ray Hartog <stevie-ray@metmeer.nl>
2015-12-13 03:09:30 +0100

    test with travis and generator php file

---
+++ generator.php
@@ -0,0 +1,48 @@
+<?php
+date_default_timezone_set('UTC');
+$date = date('Y-m-d');
+$list = array();
+$file = '.htaccess';
+
+$data = "# https://github.com/Stevie-Ray/apache-nginx-referral-spam-blacklist
+# Updated " . $date . "\r\n
+<IfModule mod_rewrite.c>\r\n
+RewriteEngine On\r\n
+";
+
+$handle = fopen("generator/domains.txt", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $line = trim(preg_replace('/\s\s+/', ' ', $line));
+        $string = "RewriteCond %{HTTP_REFERER} ^http(s)?://(www.)?.*" . $line . ".*$ [NC,OR]
+";
+        $data .= $string;
+    }
+    fclose($handle);
+} else {
+    // error opening the file.
+}
+
+$data .= "</IfModule>
+
+# Apache 2.2
+<IfModule !mod_authz_core.c>
+    <IfModule mod_authz_host.c>
+        Order allow,deny
+        Allow from all
+        Deny from env=spambot
+    </IfModule>
+</IfModule>
+
+# Apache 2.4
+<IfModule mod_authz_core.c>
+    <RequireAll>
+        Require all granted
+        Require not env spambot
+    </RequireAll>
+</IfModule>";
+
+// Write the contents back to the
+file_put_contents($file, $data);
+
+
