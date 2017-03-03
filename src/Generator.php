687522ae557664ddf0031d4aae256f70a24379d7
Justinas Urbanavicius <justinasu@gmail.com>
2017-03-03 20:58:56 +0200

    cleaned up project and composer layout

--- src/Generator.php
+++ src/Generator.php
@@ -1,20 +1,27 @@
 <?php
+namespace StevieRay;
 
 use Mso\IdnaConvert\IdnaConvert;
 
-class Generate
+class Generator
 {
-
     private $projectUrl = "https://github.com/Stevie-Ray/referrer-spam-blocker";
+
+    /** @var string string */
+    private $outputDir;
+
+    /**
+     * @param string $outputDir
+     */
+    public function __construct($outputDir)
+    {
+        $this->outputDir = $outputDir;
+    }
 
     public function generateFiles()
     {
-
-        date_default_timezone_set('UTC');
         $date = date('Y-m-d H:i:s');
-
         $lines = $this->domainWorker();
-
         $this->createApache($date, $lines);
         $this->createNginx($date, $lines);
         $this->createVarnish($date, $lines);
@@ -31,7 +38,7 @@ class Generate
         $domainsFile = __DIR__ . "/domains.txt";
 
         $handle = fopen($domainsFile, "r");
-        if (!$handle) {
+        if (! $handle) {
             throw new \RuntimeException('Error opening file ' . $domainsFile);
         }
         $lines = array();
@@ -65,15 +72,16 @@ class Generate
     }
 
     /**
-     * @param $file
-     * @param $data
+     * @param string $filename
+     * @param string $data
      */
-    protected function writeToFile($file, $data)
+    protected function writeToFile($filename, $data)
     {
+        $file = $this->outputDir . '/' . $filename;
         if (is_writable($file)) {
             file_put_contents($file, $data);
-            if (!chmod($file, 0644)) {
-                trigger_error("Couldn't not set " . basename($file) . " permissions to 644");
+            if (! chmod($file, 0644)) {
+                trigger_error("Couldn't not set " . $filename . " permissions to 644");
             }
         } else {
             trigger_error("Permission denied");
@@ -82,11 +90,10 @@ class Generate
 
     /**
      * @param string $date
-     * @param array $lines
+     * @param array  $lines
      */
     public function createApache($date, array $lines)
     {
-        $file = __DIR__ . '/../.htaccess';
         $data = "# " . $this->projectUrl . "\n# Updated " . $date . "\n\n" .
             "<IfModule mod_rewrite.c>\n\nRewriteEngine On\n\n";
         foreach ($lines as $line) {
@@ -107,16 +114,15 @@ class Generate
             "Apache 2.4\n<IfModule mod_authz_core.c>\n\t<RequireAll>" .
             "\n\t\tRequire all granted\n\t\tRequire not env spambot\n\t</RequireAll>\n</IfModule>";
 
-        $this->writeToFile($file, $data);
+        $this->writeToFile('.htaccess', $data);
     }
 
     /**
      * @param string $date
-     * @param array $lines
+     * @param array  $lines
      */
     public function createNginx($date, array $lines)
     {
-        $file = __DIR__ . '/../referral-spam.conf';
         $data = "# " . $this->projectUrl . "\n# Updated " . $date . "\n#\n# /etc/nginx/referral-spam.conf\n#\n" .
             "# With referral-spam.conf in /etc/nginx, include it globally from within /etc/nginx/nginx.conf:\n#\n" .
             "#     include referral-spam.conf;\n#\n" .
@@ -128,17 +134,15 @@ class Generate
         }
         $data .= "\n}";
 
-        $this->writeToFile($file, $data);
+        $this->writeToFile('referral-spam.conf', $data);
     }
 
     /**
      * @param string $date
-     * @param array $lines
+     * @param array  $lines
      */
     public function createVarnish($date, array $lines)
     {
-        $file = __DIR__ . '/../referral-spam.vcl';
-
         $data = "# " . $this->projectUrl . "\n# Updated " . $date . "\nsub block_referral_spam {\n\tif (\n";
         foreach ($lines as $line) {
             if ($line === end($lines)) {
@@ -151,17 +155,15 @@ class Generate
 
         $data .= "\t) {\n\t\t\treturn (synth(444, \"No Response\"));\n\t}\n}";
 
-        $this->writeToFile($file, $data);
+        $this->writeToFile('referral-spam.vcl', $data);
     }
 
     /**
      * @param string $date
-     * @param array $lines
+     * @param array  $lines
      */
     public function createIIS($date, array $lines)
     {
-        $file = __DIR__ . '/../web.config';
-
         $data = "<!-- " . $this->projectUrl . " -->\n<!-- Updated " . $date . " -->\n" .
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" .
             "<configuration>\n\t<system.webServer>\n\t\t<rewrite>\n\t\t\t<rules>\n";
@@ -175,28 +177,26 @@ class Generate
 
         $data .= "\t\t\t</rules>\n\t\t</rewrite>\n\t</system.webServer>\n</configuration>";
 
-        $this->writeToFile($file, $data);
+        $this->writeToFile('web.config', $data);
     }
 
 
     /**
      * @param string $date
-     * @param array $lines
+     * @param array  $lines
      */
     public function createuWSGI($date, array $lines)
     {
-        $file = __DIR__ . '/../referral_spam.res';
-
         $data = "# " . $this->projectUrl . "\n# Updated " . $date . "\n#\n" .
-			"# Put referral-spam.res in /path/to/vassals, then include it from within /path/to/vassals/vassal.ini:\n" .
-			"#\n# ini = referral_spam.res:blacklist_spam\n\n" .
-			"[blacklist_spam]\n";
+            "# Put referral-spam.res in /path/to/vassals, then include it from within /path/to/vassals/vassal.ini:\n" .
+            "#\n# ini = referral_spam.res:blacklist_spam\n\n" .
+            "[blacklist_spam]\n";
         foreach ($lines as $line) {
-        	$data .= "route-referer = (?i)" . preg_quote($line) . " break:403 Forbidden\n";
+            $data .= "route-referer = (?i)" . preg_quote($line) . " break:403 Forbidden\n";
         }
         $data .= "route-label = referral_spam";
 
-        $this->writeToFile($file, $data);
+        $this->writeToFile('referral_spam.res', $data);
     }
 
     /**
@@ -220,28 +220,22 @@ class Generate
         for ($x = 1; $lastPosition < $dataLength; $x++) {
 
             // already in the boundary limits?
-            if( ($dataLength-$lastPosition) >= $googleLimit){
-                // search for the last occurence of | in the boundary limits
+            if (($dataLength - $lastPosition) >= $googleLimit) {
+                // search for the last occurrence of | in the boundary limits
                 $pipePosition = strrpos(substr($data, $lastPosition, $googleLimit), '|');
 
                 $dataSplit = substr($data, $lastPosition, $pipePosition);
 
                 // without trailing pipe at the beginning of next round
-                $lastPosition = $lastPosition + $pipePosition+1;
-            }else{
+                $lastPosition = $lastPosition + $pipePosition + 1;
+            } else {
                 // Rest of the regex (no pipe at the end)
                 $dataSplit = substr($data, $lastPosition);
                 $lastPosition = $dataLength; // Break
             }
 
-            $file = __DIR__ . '/../google-exclude-' . $x . '.txt';
-            $this->writeToFile($file, $dataSplit);
+            $this->writeToFile('google-exclude-' . $x . '.txt', $dataSplit);
         }
 
     }
 }
-
-require __DIR__ . '/vendor/autoload.php';
-
-$generator = new Generate();
-$generator->generateFiles();
